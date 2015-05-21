0adde7223818c1ab03c511d1f872d04eb340f16d
yonicd <yonicd@gmail.com>
2015-05-21 18:17:18 +0300

    mega stores

--- mega_fetch.r
+++ mega_fetch.r
@@ -9,6 +9,18 @@ mega.files=mdply(c("Price","Promo","Store"),.fun = function(x){
 
 mega.files$url=as.character(mega.files$url)
 
+mega.store.files=mega.files%>%filter(type=="Store")
+
+options(warn=-1)
+mega.stores=ddply(mega.store.files,.(type,url),.fun = function(x){
+  temp <- tempfile()
+  download.file(x$url,temp,quiet = T)
+  mega.out=xmlToDataFrame(xmlParse(readLines(gzfile(temp),encoding="UTF-8")))[-1,-1]
+  unlink(temp)
+  return(mega.out)})%>%mutate_each(funs(iconv(.,"UTF-8")))
+options(warn=0)
+
+
 mega.price.files=mega.files%>%filter(type=="Price")
 
 options(warn=-1)
@@ -20,12 +32,12 @@ mega.prices=ddply(mega.price.files,.(type,url),.fun = function(x){
   return(mega.out)},.progress = "text")%>%mutate_each(funs(iconv(.,"UTF-8")))
 options(warn=0)
 
-names(mega.prices)[c(3:6)]=names(mega.stores)[c(1:4)]
+names(mega.prices)[c(3:6)]=names(mega.stores)[c(2:6)]
 
-mega.prices=left_join(mega.prices,mega.stores,by=names(mega.stores)[c(1:4)])
+mega.prices=left_join(mega.prices,mega.stores%>%select(-c(type,url)),by=names(mega.stores)[c(1:4)])
 
 
-#Promo files
+#Promo files(still working)
 mega.promo.files=mega.files%>%filter(type=="Promo")%>%slice(c(1:5))
 options(warn=-1)
 mega.promo=ddply(mega.promo.files,.(type,url),.fun = function(x){
@@ -51,5 +63,5 @@ tempdf=data.frame(value=iconv(unlist(xmlSApply(temp,xmlValue)),"UTF-8"),name=unl
 tempdf$id=rep(seq(1,nrow(tempdf)/length(unique(tempdf$name))),length(unique(tempdf$name)))
 tempdf=tempdf%>%spread(name, value)
 
-mega.promo=merge(headerdf,promo.items)
+mega.promo.main=merge(headerdf,promo.items)
 
